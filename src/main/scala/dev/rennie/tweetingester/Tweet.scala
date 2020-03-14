714ea72209fe1236f4faec3233c03d8eab0ba3e1
Gor-Ren <gor.rennie@gmail.com>
2020-03-14 00:46:03 +0000

    Add value class for tweet message

--- src/main/scala/dev/rennie/tweetingester/Tweet.scala
+++ src/main/scala/dev/rennie/tweetingester/Tweet.scala
@@ -1,6 +1,6 @@
 package dev.rennie.tweetingester
 
-import dev.rennie.tweetingester.Tweet.Id
+import dev.rennie.tweetingester.Tweet.{TweetId, TweetMessage}
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.string.ValidLong
 import io.circe.generic.semiauto.deriveEncoder
@@ -8,23 +8,32 @@ import io.circe.refined._
 import io.circe.{Decoder, Encoder, HCursor}
 import io.estatico.newtype.macros._
 
-final case class Tweet(id: Id, text: String)
+final case class Tweet(id: TweetId, text: TweetMessage)
 
 object Tweet {
-  @newtype case class Id(value: String Refined ValidLong)
+  @newtype case class TweetId(value: String Refined ValidLong)
+  @newtype case class TweetMessage(value: String)
 
-  object Id {
-    implicit val idDecoder: Decoder[Id] =
-      Decoder[String Refined ValidLong].map(Id(_))
+  object TweetId {
+    implicit val idDecoder: Decoder[TweetId] =
+      Decoder[String Refined ValidLong].map(TweetId(_))
 
-    implicit val idEncoder: Encoder[Id] =
+    implicit val idEncoder: Encoder[TweetId] =
       Encoder[String Refined ValidLong].contramap(_.value)
+  }
+
+  object TweetMessage {
+    implicit val tweetMessageDecoder: Decoder[TweetMessage] =
+      Decoder[String].map(TweetMessage(_))
+
+    implicit val tweetMessageidEncoder: Encoder[TweetMessage] =
+      Encoder[String].contramap(_.value)
   }
 
   implicit val tweetDecoder: Decoder[Tweet] = (c: HCursor) =>
     for {
-      id <- c.downField("id").as[Id]
-      text <- c.downField("text").as[String]
+      id <- c.downField("id").as[TweetId]
+      text <- c.downField("text").as[TweetMessage]
     } yield Tweet(id, text)
 
   implicit val tweetEncoder: Encoder[Tweet] = deriveEncoder[Tweet]
--- src/test/scala/dev/rennie/tweetingester/BaseTestSpec.scala
+++ src/test/scala/dev/rennie/tweetingester/BaseTestSpec.scala
@@ -1,7 +1,6 @@
 package dev.rennie.tweetingester
 
-import dev.rennie.tweetingester.Tweet.Id
-import eu.timepit.refined.api.Validate
+import dev.rennie.tweetingester.Tweet.{TweetId, TweetMessage}
 import eu.timepit.refined.refineV
 import eu.timepit.refined.string.ValidLong
 import org.scalacheck.Gen
@@ -11,15 +10,15 @@ import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 class BaseTestSpec extends FunSpec with Matchers with ScalaCheckPropertyChecks {
   val tweetMaxLength = 140
 
-  implicit val testy: Validate[String, ValidLong] = ValidLong.validLongValidate
-
-  val idGen: Gen[Id] = for {
+  val idGen: Gen[TweetId] = for {
     long <- Gen.choose(1000L, Long.MaxValue)
     id = refineV[ValidLong](long.toString).getOrElse(throw new RuntimeException)
-  } yield Id(id)
+  } yield TweetId(id)
 
   val tweetGen: Gen[Tweet] = for {
     id <- idGen
-    text <- Gen.asciiPrintableStr.suchThat(_.length < tweetMaxLength)
+    text <- Gen.asciiPrintableStr
+      .suchThat(_.length <= tweetMaxLength)
+      .map(TweetMessage(_))
   } yield Tweet(id, text)
 }
