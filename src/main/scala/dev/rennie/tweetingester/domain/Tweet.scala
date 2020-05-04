5416a2575fb5f723ea3e1ec562804cbd0531f272
Gordon <gor.rennie@gmail.com>
2020-05-04 23:28:22 +0100

    Experiment with newtype-refined-cats support

--- src/main/scala/dev/rennie/tweetingester/domain/Tweet.scala
+++ src/main/scala/dev/rennie/tweetingester/domain/Tweet.scala
@@ -1,8 +1,13 @@
 package dev.rennie.tweetingester.domain
 
 import cats.{Eq, Show}
+import cats.syntax.contravariant._
+import cats.instances.string._
+import cats.instances.eq._
 import dev.rennie.tweetingester.domain
 import dev.rennie.tweetingester.domain.Tweet.{TweetId, TweetMessage}
+import dev.rennie.tweetingester.util.NewtypeSupport.NewtypeCats
+import dev.rennie.tweetingester.util.NewtypeSupport.NewtypeCats._
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.string.ValidLong
 import eu.timepit.refined.cats._
@@ -11,7 +16,6 @@ import io.circe.generic.semiauto.deriveEncoder
 import io.circe.refined._
 import io.circe.{Decoder, Encoder, HCursor}
 import io.estatico.newtype.macros._
-import dev.rennie.tweetingester.util.NewtypeSupport.NewtypeCats._
 
 final case class Tweet(id: TweetId, text: TweetMessage)
 
@@ -19,19 +23,21 @@ object Tweet {
   @newtype case class TweetId(value: String Refined ValidLong)
   @newtype case class TweetMessage(value: String)
 
-  // TODO: why must these be summoned here? NewtypeSupport.NewtypeCats._ should be sufficient...
-  implicit val tweetIdEq: Eq[TweetId] = implicitly
-  implicit val tweetIdShow: Show[TweetId] = implicitly
-
-  implicit val tweetMessageEq: Eq[TweetMessage] = implicitly
-  implicit val tweetMessageShow: Show[TweetMessage] = implicitly
+//  implicit val tweetIdShow: Show[TweetId] = Show[String Refined ValidLong].contramap(_.value)
+//  implicit val TweetMessageShow: Show[TweetMessage] = Show[String].contramap(_.value)
+//  implicit val tweetIdEq: Eq[TweetId] = Eq.by[TweetId, String Refined ValidLong](_.value)
+//  implicit val TweetMessageEq: Eq[TweetMessage] = Eq.by[TweetMessage, String](_.value)
 
   implicit val tweetEq: Eq[Tweet] = {
+    implicit val test: Eq[TweetId] =
+      NewtypeCats.coercibleRefinedEq[Refined, String, ValidLong, TweetId]
     import cats.derived.auto.eq._
     cats.derived.semi.eq
   }
 
   implicit val tweetShow: Show[Tweet] = {
+    implicit val test2: Show[TweetId] =
+      NewtypeCats.coercibleRefinedShow[Refined, String, ValidLong, TweetId]
     import cats.derived.auto.show._
     cats.derived.semi.show
   }
--- src/main/scala/dev/rennie/tweetingester/util/NewtypeSupport.scala
+++ src/main/scala/dev/rennie/tweetingester/util/NewtypeSupport.scala
@@ -1,12 +1,17 @@
 package dev.rennie.tweetingester.util
-import cats.Eq
+import cats.{Contravariant, Eq, Show}
+import cats.instances.eq._
+import cats.syntax.contravariant._
 import cats.instances.string._
 import cats.instances.int._
+import eu.timepit.refined.api.RefType
+import eu.timepit.refined.cats.derivation._
 import io.estatico.newtype.Coercible
 import io.estatico.newtype.ops._
 
 object NewtypeSupport {
   object NewtypeCats {
+
     // credit to Gabriel Volpe: https://github.com/gvolpe/pfps-shopping-cart/
     // does not work as implicit for some reason...
     private def coercibleEq[A: Eq, B: Coercible[A, *]]: Eq[B] =
@@ -15,10 +20,31 @@ object NewtypeSupport {
           Eq[A].eqv(x.repr.asInstanceOf[A], y.repr.asInstanceOf[A])
       }
 
+    implicit def coercibleRefinedEq[F[_, _]: RefType, A: Eq, P, B: Coercible[F[A, P], *]]: Eq[B] = {
+      coercibleEq[F[A, P], B]
+    }
+
     implicit def coercibleStringEq[B: Coercible[String, *]]: Eq[B] =
       coercibleEq[String, B]
 
     implicit def coercibleIntEq[B: Coercible[Int, *]]: Eq[B] =
       coercibleEq[Int, B]
+
+    private def coercibleShow[A: Show, B: Coercible[A, *]]: Show[B] =
+      new Show[B] {
+        override def show(t: B): String = Show[A].show(t.repr.asInstanceOf[A])
+      }
+
+    implicit def coercibleStringShow[B: Coercible[String, *]]: Show[B] =
+      coercibleShow[String, B]
+
+    implicit def coercibleIntShow[B: Coercible[Int, *]]: Show[B] =
+      coercibleShow[Int, B]
+
+    implicit def coercibleRefinedShow[F[_, _]: RefType, A: Show, P, B: Coercible[F[A, P], *]]
+        : Show[B] = {
+      implicit val fShow: Show[F[A, P]] = refTypeViaContravariant[F, Show, A, P]
+      coercibleShow[F[A, P], B]
+    }
   }
 }
